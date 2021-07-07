0bd78210439da2da4f058cc80af8212ec6ad6e87
Sid Ajay <>
2021-07-06 17:13:44 -0700

    No Comments

--- Assets/scripts/movebot.cs
+++ Assets/scripts/movebot.cs
@@ -14,7 +14,17 @@ public class movebot : MonoBehaviour
     void Update()
     {
         if(Input.GetKey(KeyCode.UpArrow)){
-            transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z+10);
+            transform.position = new Vector3(transform.position.x+1,transform.position.y,transform.position.z);
             }
+        if(Input.GetKey(KeyCode.DownArrow)){
+            transform.position = new Vector3(transform.position.x-1,transform.position.y,transform.position.z);
+            }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z-1);
+            }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z+1);
+            }
+
     }
 }
